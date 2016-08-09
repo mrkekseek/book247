10ffad31fe0113df8dfd6057855c7420f434e592
Stefan Bogdan <stefan.bogdan@ymail.com>
2016-08-09 10:41:24 +0300

    Fix #2 for migration

--- database/migrations/2016_08_02_111601_create_user_memberships_table.php
+++ database/migrations/2016_08_02_111601_create_user_memberships_table.php
@@ -14,8 +14,6 @@ class CreateUserMembershipsTable extends Migration
     {
         Schema::create('user_memberships', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->nullable();
-            $table->integer('membership_id')->unsigned()->nullable();
             $table->date('day_start');
             $table->date('day_stop');
             $table->string('membership_name');
@@ -23,14 +21,18 @@ class CreateUserMembershipsTable extends Migration
             $table->float('price');
             $table->float('discount');
             $table->longText('membership_restrictions');
-            $table->integer('signed_by')->unsigned()->nullable();       // the employee that assigned the membership or the user himself
             $table->enum('status', ['active', 'suspended', 'canceled', 'expired']);
             $table->timestamps();
         });
 
         Schema::table('user_memberships', function($table){
+            $table->integer('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('signed_by')->unsigned()->nullable();       // the employee that assigned the membership or the user himself
             $table->foreign('signed_by')->references('id')->on('users');
+
+            $table->integer('membership_id')->unsigned()->nullable();
             $table->foreign('membership_id')->references('id')->on('membership_plans');
         });
     }
