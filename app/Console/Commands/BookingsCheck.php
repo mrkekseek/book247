4eff91158b362419f9dcafdfdb40e37a07086f1f
Stefan Bogdan <stefan.bogdan@ymail.com>
2016-09-08 13:06:59 +0300

    Added cron job for bookings update - bookings that are in the past will automatically update to old, paid, unpaid statuses

---
+++ app/Console/Commands/BookingsCheck.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\BookingInvoice;
+use Illuminate\Console\Command;
+use App\Booking;
+
+class BookingsCheck extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'booking:check_past_bookings';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This will search for bookings that have a starting date in the past and are still active and change the status accordingly.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $bookings = Booking::where('status','=','active')->get();
+        if ($bookings){
+            $updated_bookings = 0;
+            foreach($bookings as $booking){
+                // paid -   booking that has an invoice and is paid
+                // unpaid - booking that has an invoice and is not paid
+                // old -    booking that is membership free
+
+                switch ($booking->payment_type) {
+                    case 'cash' :
+                        $invoiceID = $booking->invoice_id;
+                        $invoice = BookingInvoice::where('id','=',$invoiceID)->get();
+
+                        if (!$invoice){
+                            continue;
+                        }
+
+                        if ($invoice->status == "completed"){
+                            $booking->status = 'paid';
+                        }
+                        else{
+                            $booking->status = 'unpaid';
+                        }
+
+                        break;
+                    case 'membership' :
+                        $booking->status = 'old';
+                        break;
+                    case 'recurring':
+                        $booking->status = 'old';
+                        break;
+                }
+
+                $booking->save();
+            }
+        }
+
+
+    }
+}
