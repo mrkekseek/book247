9c989a326b1f73bbd017ce7bf589af0edfb64f21
Stefan Bogdan <stefan.bogdan@ymail.com>
2016-11-24 02:04:37 +0200

    Seeder updated for roles and permissions

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -222,5 +222,84 @@ class PermissionsTableSeeder extends Seeder
                 'created_at'    => Carbon::now()->format('Y-m-d H:i:s')
             ]);
         }
+
+        // now we add permissions to existing roles
+        $permission_role = [
+            // owner role
+            ['permission_id'=>1,'role_id'=>1],
+            ['permission_id'=>2,'role_id'=>1],
+            ['permission_id'=>3,'role_id'=>1],
+            ['permission_id'=>4,'role_id'=>1],
+            ['permission_id'=>5,'role_id'=>1],
+            ['permission_id'=>6,'role_id'=>1],
+            ['permission_id'=>7,'role_id'=>1],
+            ['permission_id'=>8,'role_id'=>1],
+            ['permission_id'=>9,'role_id'=>1],
+            ['permission_id'=>10,'role_id'=>1],
+            ['permission_id'=>11,'role_id'=>1],
+            ['permission_id'=>12,'role_id'=>1],
+            ['permission_id'=>13,'role_id'=>1],
+            ['permission_id'=>14,'role_id'=>1],
+            ['permission_id'=>15,'role_id'=>1],
+            ['permission_id'=>16,'role_id'=>1],
+            ['permission_id'=>17,'role_id'=>1],
+            ['permission_id'=>18,'role_id'=>1],
+            ['permission_id'=>19,'role_id'=>1],
+            ['permission_id'=>20,'role_id'=>1],
+
+            // manager role
+            ['permission_id'=>5,'role_id'=>2],
+            ['permission_id'=>6,'role_id'=>2],
+            ['permission_id'=>7,'role_id'=>2],
+            ['permission_id'=>8,'role_id'=>2],
+            ['permission_id'=>9,'role_id'=>2],
+            ['permission_id'=>10,'role_id'=>2],
+            ['permission_id'=>11,'role_id'=>2],
+            ['permission_id'=>12,'role_id'=>2],
+            ['permission_id'=>13,'role_id'=>2],
+            ['permission_id'=>14,'role_id'=>2],
+            ['permission_id'=>15,'role_id'=>2],
+            ['permission_id'=>16,'role_id'=>2],
+            ['permission_id'=>17,'role_id'=>2],
+            ['permission_id'=>18,'role_id'=>2],
+            ['permission_id'=>19,'role_id'=>2],
+            ['permission_id'=>20,'role_id'=>2],
+
+            // financial manager
+            ['permission_id'=>1,'role_id'=>3],
+            ['permission_id'=>3,'role_id'=>3],
+            ['permission_id'=>5,'role_id'=>3],
+            ['permission_id'=>6,'role_id'=>3],
+            ['permission_id'=>7,'role_id'=>3],
+            ['permission_id'=>8,'role_id'=>3],
+            ['permission_id'=>9,'role_id'=>3],
+            ['permission_id'=>10,'role_id'=>3],
+            ['permission_id'=>11,'role_id'=>3],
+            ['permission_id'=>20,'role_id'=>3],
+
+            // simple employee
+            ['permission_id'=>5,'role_id'=>4],
+            ['permission_id'=>6,'role_id'=>4],
+            ['permission_id'=>7,'role_id'=>4],
+            ['permission_id'=>8,'role_id'=>4],
+            ['permission_id'=>9,'role_id'=>4],
+            ['permission_id'=>20,'role_id'=>4],
+
+            // system agent
+            ['permission_id'=>5,'role_id'=>7],
+            ['permission_id'=>6,'role_id'=>7],
+            ['permission_id'=>8,'role_id'=>7],
+            ['permission_id'=>9,'role_id'=>7],
+            ['permission_id'=>20,'role_id'=>7],
+        ];
+        foreach($permission_role as $pr){
+            if (!DB::table('permission_role')->where('permission_id','=',$pr['permission_id'])->where('role_id','=',$pr['role_id'])->get()){
+                // not found, make insert
+                DB::table('permission_role')->insert([
+                    'permission_id' => $pr['permission_id'],
+                    'role_id'       => $pr['role_id'],
+                ]);
+            }
+        }
     }
 }
--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -14,41 +14,48 @@ class RolesTableSeeder extends Seeder
     public function run()
     {
         DB::table('roles')->insert([
+            'id'            => 1,
             'name'          => 'owner',
             'display_name'  => 'Business Owner/Partner',
             'description'   => 'This user role has full rights over the account',
         ]);
 
         DB::table('roles')->insert([
+            'id'            => 2,
             'name'          => 'manager',
             'display_name'  => 'Business Manager',
             'description'   => 'This user role has restrictions over the finance part of the account',
         ]);
 
         DB::table('roles')->insert([
+            'id'            => 3,
             'name'          => 'finance',
             'display_name'  => 'Finance Accountant',
             'description'   => 'This user role has restrictins to the administrative part of the account',
         ]);
 
         DB::table('roles')->insert([
+            'id'            => 4,
             'name'          => 'employee',
             'display_name'  => 'Employee',
             'description'   => 'This user role has most of the restrictions and can do only the basic actions',
         ]);
 
         DB::table('roles')->insert([
+            'id'            => 5,
             'name'          => 'front-member',
             'display_name'  => 'Registered User with Membership',
             'description'   => 'A registered front end user with membership',
         ]);
 
         DB::table('roles')->insert([
+            'id'            => 6,
             'name'          => 'front-user',
             'display_name'  => 'Registered User without Membership',
             'description'   => 'A registered front end user without membership, on a pay-as-you-go status',
         ]);
         DB::table('roles')->insert([
+            'id'            => 7,
             'name'          => 'system-agent',
             'display_name'  => 'System Agent',
             'description'   => 'Sends messages to users/members - the automated process',
