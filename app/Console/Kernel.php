a74c51de30f14207c805caaf7a24863c8eb78726
Stefan Bogdan <stefan.bogdan@ymail.com>
2017-03-08 01:56:43 +0200

    Small change in kernel command code.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -55,8 +55,9 @@ class Kernel extends ConsoleKernel
             //->everyFiveMinutes()
             ->timezone('Europe/Oslo')
             ->when(function(){
-                  $current_hour     = Carbon::now('Europe/Oslo')->format('H');
-                  $current_minute   = Carbon::now('Europe/Oslo')->format('i');
+                  $current_hour     = intval(Carbon::now('Europe/Oslo')->format('H'));
+                  $current_minute   = intval(Carbon::now('Europe/Oslo')->format('i'));
+                  
                   return $current_hour >= 22 && $current_minute > 4 && $current_hour < 6;
             })
             ->appendOutputTo('storage/logs/PendingInvoice_output.log');
