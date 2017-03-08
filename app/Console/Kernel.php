3a6a0eeec9f6e4733b0c41b089dd8f696ace969e
Stefan Bogdan <stefan.bogdan@ymail.com>
2017-03-08 02:40:15 +0200

    Kernel schedule fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -57,7 +57,12 @@ class Kernel extends ConsoleKernel
                   $current_hour     = intval(Carbon::now('Europe/Oslo')->format('H'));
                   $current_minute   = intval(Carbon::now('Europe/Oslo')->format('i'));
 
-                  return $current_hour >= 22 && $current_minute > 4 && $current_hour < 6;
+                  if (($current_hour>=22 && $current_minute>4) || ($current_minute>4 && $current_hour<6)){
+                      return true;
+                  }
+                  else{
+                      return false;
+                  }
             })
             ->appendOutputTo('storage/logs/PendingInvoice_output.log');
 
