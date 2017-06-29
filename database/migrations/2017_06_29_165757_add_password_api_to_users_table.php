a85f56fea191ba2f4164086776a7fbc61e96ad97
Stefan Bogdan <stefan.bogdan@ymail.com>
2017-06-29 18:10:57 +0300

    Added migration for sso_password field in users table.

---
+++ database/migrations/2017_06_29_165757_add_password_api_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPasswordApiToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->char('password_api',60)->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('password_api');
+        });
+    }
+}
