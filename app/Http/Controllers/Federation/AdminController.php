82547db266b999b65028530e2fe6e900ed9c2cf3
Vladimir <vladimirdbalan@gmail.com>
2017-06-30 19:21:08 +0300

    Ipn and sso user import.

--- app/Http/Controllers/Federation/AdminController.php
+++ app/Http/Controllers/Federation/AdminController.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\MessageBag;
 use Carbon\Carbon;
 use App\Http\Controllers\Api;
+use App\Http\Controllers\Federation\AnyApi;
+use App\Http\Libraries\ApiAuth;
 
 class AdminController extends Base
 {
@@ -32,11 +34,15 @@ class AdminController extends Base
 //        return json_encode(Api::send_curl(['memberSSOid'=>5 , 'country'=> 'NO' ,'activity' => 1],'federation_member_has_valid_license','GET'));
 //        return json_encode(Api::send_curl(['account_key' => '11381-46565-30640-84804-20809'],'validate_account_key','POST'));
 //        return Api::send_curl(['memberSSOid' => 15 ,'activity' => null],'federation_buy_license','POST');
+//        return ApiAuth::accounts_get(124);
+//        $x = ApiAuth::send('','http://rankedinbookingsso-test.azurewebsites.net/api/Accounts/136','GET');
+//        $x = AnyApi::send_curl('http://rankedinbookingsso-test.azurewebsites.net',[],'/api/Accounts/28','GET');
+
 
         if($r->method() == "POST"){
             return json_encode($r->all());
         }
-        $result =  Api::send_curl(['memberSSOid' => 115 ,'membership_id' => null, 'account_key' => '81381-46565-30640-84804-20809', 'return_url' => "http://book.net/admin/test_api_call" ],'federation_buy_license','POST');
+        $result =  Api::send_curl(['memberSSOid' => 125 ,'membership_id' => null, 'account_key' => '81381-46565-30640-84804-20809', 'return_url' => "http://book.net/admin/test_api_call" ],'federation_buy_license','POST');
         if(isset($result->iFrameUrl)) {
             return view('development',['link' => $result->iFrameUrl]);
         } else {
@@ -81,7 +87,6 @@ class AdminController extends Base
             $data = $r->all();
             unset($data['method']);
             $response = Api::send_curl($data, $method, $r->method());
-//            dd($response);
             if ($response->code == 1) {
                 return json_encode(array(
                         'success' => true ,
