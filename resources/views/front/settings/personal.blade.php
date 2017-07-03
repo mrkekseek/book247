216df1b35075c303b5609087e0bd6e8e0933ef19
Alex Kovalchuk <mejison8@gmail.com>
2017-07-03 16:17:50 +0300

    Rewrite text

--- resources/views/front/settings/personal.blade.php
+++ resources/views/front/settings/personal.blade.php
@@ -256,7 +256,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title">Remove avtar</h4>
+                    <h4 class="modal-title">Remove avatar</h4>
                 </div>
                 <div class="modal-body">
                     <p>Do you want to remove avatar?</p>
