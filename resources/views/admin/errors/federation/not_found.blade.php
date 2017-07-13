f71eb81f06a3755fb72987463414374cec6e54fc
Vladimir <vladimirdbalan@gmail.com>
2017-07-13 11:30:00 +0300

    Permission denied routes.

---
+++ resources/views/admin/errors/federation/not_found.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.layouts.federation.main')
+
+@section('pageLevelPlugins')
+@endsection
+
+@section('themeGlobalStyle')
+    <link href="{{ asset('assets/global/css/components-rounded.min.css') }}" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="{{ asset('assets/global/css/plugins.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/pages/css/error.min.css') }}" rel="stylesheet" type="text/css" />
+@endsection
+
+@section('themeLayoutStyle')
+    <link href="{{ asset('assets/layouts/layout4/css/layout.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/layouts/layout4/css/themes/light.min.css') }}" rel="stylesheet" type="text/css" id="style_color" />
+    <link href="{{ asset('assets/layouts/layout4/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+@endsection
+
+@section('title', 'Back-end users - All User Roles')
+@section('pageBodyClass','page-container-bg-solid page-header-fixed page-sidebar-closed-hide-logo')
+
+@section('pageContentBody')
+    <div class="page-content">
+        <!-- BEGIN PAGE HEAD-->
+        <div class="page-head">
+            <!-- BEGIN PAGE TITLE -->
+            <div class="page-title">
+                <h1>Page not found
+                    <small>404 error</small>
+                </h1>
+            </div>
+            <!-- END PAGE TITLE -->
+        </div>
+        <!-- END PAGE HEAD-->
+        <!-- BEGIN PAGE BASE CONTENT -->
+        <div class="row">
+            <div class="col-md-12 page-500 margin-top-40">
+                <div class=" number font-red"> Oops! </div>
+                <div class=" details">
+                    <h3> The requested page could not be found</h3>
+                    <p> This error was logged and will be investigated by the administrators
+                        <br> </p>
+                    <p>
+                        If this is the first time you see this, try logout/login then access teh link again.
+                        <br> </p>
+                </div>
+            </div>
+        </div>
+        <!-- END PAGE BASE CONTENT -->
+    </div>
+@endsection
+
+@section('pageBelowLevelPlugins')
+@endsection
+
+@section('pageBelowLevelScripts')
+    <script src="{{ asset('assets/global/plugins/jquery-notific8/jquery.notific8.min.js') }}" type="text/javascript"></script>
+@endsection
+
+@section('themeBelowLayoutScripts')
+    <script src="{{ asset('assets/layouts/layout4/scripts/layout.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/layouts/layout4/scripts/demo.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
+@endsection
+
+@section('pageCustomJScripts')
+    <script type="text/javascript">
+
+    </script>
+@endsection
