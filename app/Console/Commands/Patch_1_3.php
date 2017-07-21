6308b6d9145d93c7a0ade82806eebc859185c14c
Stefan Bogdan <stefan.bogdan@ymail.com>
2017-07-21 10:10:12 +0300

    Patch 1.3 - code optimisation and showing update.

--- app/Console/Commands/Patch_1_3.php
+++ app/Console/Commands/Patch_1_3.php
@@ -40,17 +40,29 @@ class Patch_1_3 extends Command
      */
     public function handle()
     {
-        $booking = Booking::with('resource')->orderBy('date_of_booking','ASC')->get();
+        echo 'Enter patch 1.3 '.PHP_EOL;
+        $nr = 0;    $all = 0;
+
+        $booking = Booking::with('resource')->orderBy('date_of_booking','ASC')->groupBy('for_user_id','resource_id')->get();
         foreach ($booking as $item)
         {
+            $all++;
             $userBookedActivity = UserBookedActivity::firstOrNew([
                 'user_id' => $item->for_user_id,
                 'activity_id' => $item->resource->category_id,
             ]);
+
             if ( ! $userBookedActivity->exists )
             {
+                $nr++;
                 $userBookedActivity->save();
+
+                if ($nr%100 == 0){
+                    echo 'Found so far : '.$nr.' new users without activities. Processed : '.$all.' @@ '.PHP_EOL;
+                }
             }
         }
+
+        echo 'Exiting patch 1.3 - processed : '.$all.' and found new : '.$nr.' membebers that were added to activity. '.PHP_EOL;
     }
 }
