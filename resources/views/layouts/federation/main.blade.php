7eb861cf35c0e1c28e7c488147b75ca6cf597a75
Vladimir <vladimirdbalan@gmail.com>
2017-08-15 18:04:53 +0300

    Front user membership buy fixes.

--- resources/views/layouts/federation/main.blade.php
+++ resources/views/layouts/federation/main.blade.php
@@ -78,21 +78,29 @@
 <script src="{{ asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
 <!-- END CORE PLUGINS -->
 <!-- BEGIN PAGE LEVEL PLUGINS -->
+
 @yield('pageBelowLevelPlugins')
+
 <!-- END PAGE LEVEL PLUGINS -->
 <!-- BEGIN THEME GLOBAL SCRIPTS -->
 <script src="{{ asset('assets/global/scripts/app.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('assets/global/scripts/admin-scripts.js') }}" type="text/javascript"></script>
 <!-- END THEME GLOBAL SCRIPTS -->
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
+
 @yield('pageBelowLevelScripts')
+
 <!-- END PAGE LEVEL SCRIPTS -->
 <!-- BEGIN THEME LAYOUT SCRIPTS -->
+
 @yield('themeBelowLayoutScripts')
+
 <!-- END THEME LAYOUT SCRIPTS -->
 
 <!-- BEGIN THEME LAYOUT SCRIPTS -->
+
 @yield('pageCustomJScripts')
+
 <!-- END THEME LAYOUT SCRIPTS -->
 
 </body>
