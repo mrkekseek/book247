4db3d4fb666927c63898319b57ac10c236438cb6
Vladimir <vladimirdbalan@gmail.com>
2017-09-07 12:14:09 +0300

    Refactored clear pending bookings.

--- app/Console/Commands/ClearBookings.php
+++ app/Console/Commands/ClearBookings.php
@@ -36,37 +36,18 @@ class ClearBookings extends Command
         if (AppSettings::get_setting_value_by_name('bookings_online_payment_rule')) {
 
             $bookings = Booking::where('status','active')->get();
-            $bookings_array = [];
-            foreach ($bookings as $booking) {
-                $bookings_array[$booking->id] = $booking;
-                $to_check[$booking->id] = true;
-            }
 
             foreach ($bookings as $booking) {
-                if (isset($to_check[$booking->id])) {
-                    //time check
+                //time check
+                if ($booking->invoice_id > 0) {
                     $booking_date = Carbon::createFromFormat("Y-m-d H:i:s",$booking->created_at);
                     $now = date("Y-m-d H:i:s");
                     $now = Carbon::createFromFormat("Y-m-d H:i:s",$now);
                     if ($booking_date->addMinutes(10)->lt($now) && User::find($booking->by_user_id)->is_front_user()) {
-                        $booking_invoice = BookingInvoice::where('booking_id',$booking->id)->first();
-                        if ($booking_invoice) {;
-                            $booking_invoice_items = BookingInvoiceItem::where('booking_invoice_id',$booking_invoice->id)->get();
-                            if ($booking_invoice->status == 'completed') {
-                                foreach ($booking_invoice_items as $item) {
-                                    unset($to_check[$item->booking_id]);
-                                }
-                            } else if ($booking_invoice->status == 'pending') {
-                                foreach ($booking_invoice_items as $item) {
-                                    $bookings_array[$item->booking_id]->cancel_booking();
-                                    unset($to_check[$item->booking_id]);
-                                }
-                            } else if ($booking_invoice->status == 'cancelled') {
-                                foreach ($booking_invoice_items as $item) {
-                                    $bookings_array[$item->booking_id]->cancel_booking();
-                                    unset($to_check[$item->booking_id]);
-                                }
-                            }
+                        if ($booking->invoice->status == 'pending') {
+                            $booking->cancel_booking();
+                        } else if ($booking->invoice->status == 'cancelled') {
+                            $booking->cancel_booking();
                         }
                     }
                 }
--- app/Http/Controllers/Federation/AdminController.php
+++ app/Http/Controllers/Federation/AdminController.php
@@ -72,6 +72,7 @@ class AdminController extends Base
 
 
 
+
     public function ajaxPay(Request $r) {
 
     }
